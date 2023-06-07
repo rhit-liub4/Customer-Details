b9aa375517a457dae22b95eb13975f9e099e6318
liub4 <liub4@rose-hulman.edu>
2023-06-07 13:01:59 -0400

    js for front end lol
    Co-authored-by: rhit-hueyee <rhit-hueyee@users.noreply.github.com>

---
+++ frontend/scripts/main.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    $("#userForm").submit(function (event) {
+        event.preventDefault();
+        $('#errorTextBox').empty();
+        var formData = {
+            fname: $("#fname").val(),
+            lname: $("#lname").val(),
+            favoriteColor: $("#favcolor").val(),
+            dateOfBirth: $("#dob").val(),
+            email: $("#email").val(),
+            phone: $("#phone").val(),
+        };
+
+        var errors = [];
+
+        if (!formData.fname) {
+            errors.push("fname");
+        }
+        if (!formData.lname) {
+            errors.push("lname");
+        }
+        if (!formData.favoriteColor) {
+            errors.push("favcolor");
+        }
+        if (!formData.dateOfBirth) {
+            errors.push("dob");
+        }
+        if (!formData.email) {
+            errors.push("email");
+        }
+        if (!formData.phone) {
+            errors.push("phone");
+        }
+
+
+        if (errors.length > 0) {
+            for (i = 0; i < errors.length; i++) {
+                var error = errors[i];
+                $('#' + errors[i]).addClass('invalid');
+                console.log(document.getElementById("#" + errors[i]+"L"));
+
+                let target =errors[i]+"L"
+                
+                let text = document.getElementById(target).innerText;
+
+
+                $('#errorTextBox').append("<div>The field: " + text + " is invalid</div>");
+            }
+            return;
+        }
+
+
+        $.ajax({
+            type: "POST",
+            url: "/",
+            data: formData,
+            dataType: "json",
+            encode: true,
+        }).done(function (data) {
+            console.log(data);
+        });
+
+        
+        event.stopPropagation();
+    });
+});
